b84e70a9c24efd7aafbd09edb5d9188913b22c34
Zebbb <geraldvillaceran01@gmail.com>
2025-05-26 13:24:20 +0800

    feat: remove search bar

--- app/admin/applications/applicants/page.tsx
+++ app/admin/applications/applicants/page.tsx
@@ -91,44 +91,6 @@ const ApplicantPage = () => {
                 </p>
             </div>
 
-            <div className="flex justify-end gap-3 mb-3">
-                <ListBoxComponent
-                    label="Filter by Benefits"
-                    options={benefits}
-                    selected={selected}
-                    onChange={setSelected}
-                    getLabel={(person) => person.name}
-                    getKey={(person) => person.id}
-                />
-
-                <div className="relative mt-5">
-                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                        <svg
-                            className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 20 20"
-                        >
-                            <path
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                            />
-                        </svg>
-                    </div>
-                    <input
-                        type="search"
-                        id="default-search"
-                        className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Search First Name, Last Name..."
-                        required
-                    />
-                </div>
-            </div>
-
             {benefitApplicationQuery.isLoading ? (
                 <div className="py-10 text-gray-500 text-lg">Loading applications...</div>
             ) : benefitApplicationQuery.isError ? (
